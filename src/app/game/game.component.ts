555781be41a57ff5253f3002671eb1455c0af459
Daniel Sinteck <d.sint3ck@gmail.com>
2024-03-23 00:49:40 +0100

    ++ card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,17 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard?: string = '';
+  game!: Game;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
 }
